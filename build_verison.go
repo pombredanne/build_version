9d233bf40c4c723ea61a553589b5c8117929ad8f
Will Fitzgerald <willf@github.com>
2017-06-26 13:58:47 -0400

    add tag and documentation

--- build_verison.go
+++ build_verison.go
@@ -4,10 +4,12 @@ import "fmt"
 import "runtime"
 
 var (
-	// BuildVersion gets set to the current git SHA while building
+	// BuildVersion gets set to the current git SHA
 	BuildVersion = "unknown"
+	// Tag also gets set to the most recent tag
+	Tag = "unknown"
 )
 
 func main() {
-	fmt.Printf("Hello! Build version is %s, running on %s/%s\n", BuildVersion, runtime.GOOS, runtime.GOARCH)
+	fmt.Printf("Hello! Latest tag is %s, Build version is %s, built for %s/%s\n", Tag, BuildVersion, runtime.GOOS, runtime.GOARCH)
 }
